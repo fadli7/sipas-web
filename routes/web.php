9265122d34b29bfca2fadda11c852b4a980d1de0
azissadakah <abdulazissadakah@gmail.com>
2023-05-09 17:01:48 +0800

    [CREATE} dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers;
 
 Auth::routes();
 
@@ -9,6 +10,7 @@ Route::get('/', function () {
     return view('index');
 })->name('landing');
 
+Route::get('/dashboard',[Controllers\DashboardController::class,'index'] )->name('dashboard');
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
