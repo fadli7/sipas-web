d20a8e2658875665765f84e32984adf3bf392063
azissadakah <abdulazissadakah@gmail.com>
2023-05-09 14:27:17 +0800

    [CREATE] Login Page [CREATE] Register Page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('landing');
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
